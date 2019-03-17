985776ff3995d5ce2cd27c16d2e7759f9b4f96aa
danielle.lasauskas <daniellelasauskas@gmail.com>
2019-03-17 09:48:39 -0500

    updated files

---
+++ to-do-app/app.js
@@ -0,0 +1,9 @@
+function onReady() {
+  const addToDoForm = document.getElementByID('addToDoForm');
+  const newToDoText = document.getElementByID('newToDoText');
+  const toDoList = document.getElementByID('toDoList');
+}
+window.onload = function () {
+  alert("The window has loaded!");
+  onReady();
+};
